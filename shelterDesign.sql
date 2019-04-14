9c000d38b4f3427fa847d483e1b323fc6db505cf
cmart1997 <christmartinez1997@yahoo.com>
2019-04-13 23:56:22 -0700

    added SQLite database for the forms

---
+++ shelterDesign.sql
@@ -0,0 +1,28 @@
+CREATE TABLE front_form 
+(Caller_Name TEXT, 
+Caller_Phone TEXT,
+Caller_Role TEXT,
+Staff_Name TEXT, 
+Date_of_file DATE,
+Guardian_Phone TEXT,
+Reason_for_call TEXT
+Youth_Name TEXT, 
+Youth_Age INTEGER,
+Youth_Birthday DATE, 
+Youth_Phone TEXT, 
+Reason_For_Contact TEXT, 
+Referalls_Provided TEXT);
+
+
+
+
+CREATE_TABLE back_form 
+(Reason_For_Shelter TEXT, 
+Behavior_Questionaire TEXT, 
+Social_Worker_Assigned INTEGER, 
+Social_Worker_Name TEXT, 
+Contact_Parent_Question TEXT, 
+Qualify_For_shelter INTEGER, 
+Shelter_Date_And_Time TEXT, 
+Denied_Shelter_Reason TEXT
+);
